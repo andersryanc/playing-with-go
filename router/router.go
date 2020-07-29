a63f9fd8e8e3b24c576b7569db852f33476515ab
Ryan Anderson <anderson.ryanc@gmail.com>
2020-07-28 18:32:28 -0700

    remove old sample routes

--- router/router.go
+++ router/router.go
@@ -17,13 +17,6 @@ func Router(conn *pgx.Conn) (*mux.Router, error) {
 
 	router := mux.NewRouter()
 
-	// router.HandleFunc("/api/task", middleware.GetAllTask).Methods("GET", "OPTIONS")
-	// router.HandleFunc("/api/task", middleware.CreateTask).Methods("POST", "OPTIONS")
-	// router.HandleFunc("/api/task/{id}", middleware.TaskComplete).Methods("PUT", "OPTIONS")
-	// router.HandleFunc("/api/undoTask/{id}", middleware.UndoTask).Methods("PUT", "OPTIONS")
-	// router.HandleFunc("/api/deleteTask/{id}", middleware.DeleteTask).Methods("DELETE", "OPTIONS")
-	// router.HandleFunc("/api/deleteAllTask", middleware.DeleteAllTask).Methods("DELETE", "OPTIONS")
-
 	router.HandleFunc("/", m.Handler).Methods("GET", "OPTIONS")
 	router.HandleFunc("/users/{id:[0-9]+}", m.GetUserByIDHandler).Methods("GET", "OPTIONS")
 	router.HandleFunc("/users", m.GetAllUsersHandler).Methods("GET", "OPTIONS")
